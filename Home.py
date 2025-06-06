46cadd73689f9104c16d429b144d14d932ec783a
PI-TidalHealth <annajirao.challa@tidalhealth.org>
2025-06-06 14:20:22 +0000

    change

--- Home.py
+++ Home.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from modules.upload import render_upload_page
 from modules.sce3 import week_analysis
-from modules.sce2 import month_analysis  # 确保路径正确
+from modules.sce2 import month_analysis 
 from modules.sce4 import duration_month_analysis
 from modules.sce5 import duration_week_analysis
 from modules.step1 import uploadstep1_page
 import streamlit as st 
-#Streamlit 页面配置
+#Streamlit 
 st.set_page_config(
     page_title="HORA",
     page_icon="assets/HORA-Webpage-Icon.png",
     layout="wide" 
 )
 
-#保证session_state.page有一个初始值
+
 if "page" not in st.session_state:
     st.session_state.page = "Upload"
 if st.session_state.page == "Upload":
@@ -21,7 +21,6 @@ if st.session_state.page == "Upload":
 elif st.session_state.page== "step1":
     uploadstep1_page()
 # if session_state.page == "Upload", we will run the function "render_upload_page()"
-#以此类推
 elif st.session_state.page == "sce2":
     month_analysis()
 elif st.session_state.page == "sce3":
--- modules/upload.py
+++ modules/upload.py
@@ -7,26 +7,30 @@ import plotly.express as px
 from pathlib import Path
 from modules.layout import set_narrow,set_fullwidth
 def render_upload_page():
+
     set_narrow(800)
     ROOT = Path(__file__).resolve().parents[1]
-    #找到root目录
-    LOGO = ROOT / "assets" / "Logo1.PNG"
-    #这样做的目的是找到项目的根目录
 
-    st.image(
-        str(LOGO),
-        use_container_width=True,
-    )
+    LOGO = ROOT / "assets" / "Logo4.PNG"
+    col_left, col_middle, col_right = st.columns([1, 12, 1])
+    with col_middle:
+        # This is your logo
+        st.image(str(LOGO), use_container_width=True)
 
-    uploaded = st.file_uploader("Upload a file", type=["csv","xlsx"], key="crna_uploader")
-    #KEY是用来确保每次上传文件时，文件名都是唯一的,用于在 session_state 中存储上传状态
+        # This is the single file uploader (do NOT duplicate it below)
+        uploaded = st.file_uploader(
+            "Upload a file", 
+            type=["csv", "xlsx"], 
+            key="crna_uploader"
+        )
+
     if not uploaded:
       return
 
-    # 使用5列布局，中间列放按钮
+
     btn_col1, btn_col2, btn_col3, btn_col4, btn_col5 = st.columns([2,2,1,2,2])
-    with btn_col3:  # 使用中间列
-        if st.button("Next ➡️", use_container_width=True):  # 添加 use_container_width=True 使按钮填充列宽
+    with btn_col3:  
+        if st.button("Next ➡️", use_container_width=True):  
             st.session_state.uploaded_file = uploaded
             st.session_state.page = "step1"
             st.rerun()
